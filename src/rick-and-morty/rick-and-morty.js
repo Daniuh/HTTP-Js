98895a0759a0a81806283b87c86eede2758c497c
Daniel Naranjo <naranjobenavidesd@gmail.com>
2025-03-25 07:36:49 +0100

    Se hace el evento click para que el usuario pueda llamar otra petición http

--- src/rick-and-morty/rick-and-morty.js
+++ src/rick-and-morty/rick-and-morty.js
@@ -23,18 +23,27 @@ const fetchQuote = async() => {
 export const RickAndMorty = async(element) => {
 
     document.querySelector('#app-title').innerHTML = 'Rick y Morty';
-    element.innerHTML = 'Cargando...';
     //const quote = await fetchQuote();
     const quoteLabel = document.createElement('blockquote');
     const authoLabel = document.createElement('h3');
     const nextButtom = document.createElement('button');
     nextButtom.innerText = 'Next Quote';
-
+/*     element.replaceChildren(nextButtom);
+ */
     const renderQuote = (data) => {
         quoteLabel.innerHTML = data.name;
         authoLabel.innerHTML = data.gender;
         element.replaceChildren(quoteLabel, authoLabel, nextButtom);
     }
+
+    nextButtom.addEventListener('click', async () => {
+        /* nextButtom.disabled = true;
+        element.innerHTML = 'Cargando...';
+        fetchQuote().then(renderQuote);
+        nextButtom.disabled = false; */
+        element.innerHTML = 'Cargando...';
+        renderQuote(await fetchQuote());
+    });
 
     fetchQuote().then(renderQuote);
 }
